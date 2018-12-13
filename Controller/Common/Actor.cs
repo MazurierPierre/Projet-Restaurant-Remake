daa78e9e5a5e5146fcf693b77ca29c1cee2f58d3
Pierre Mazurier <pierre.mazurier@viacesi.fr>
2018-12-13 16:54:34 +0100

    Before remove branch

---
+++ Controller/Common/Actor.cs
@@ -0,0 +1,27 @@
+﻿using Controller.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Controller.Common
+{
+    class Actor : IActor, IObserver<T>
+    {
+        public void OnCompleted()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void OnError(Exception error)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void OnNext(T value)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
