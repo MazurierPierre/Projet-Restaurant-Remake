769b2d0bd815cb785c6ed0af9903a8a53dc6949d
Unknown <emilien.pons@viacesi.fr>
2018-12-13 17:27:34 +0100

    Add all empty model classes

--- Model/Kitchen/Interfaces/IStorage.cs
+++ Model/Kitchen/Interfaces/IStorage.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Model
+namespace Model.Kitchen.Interfaces
 {
-    class Class1
+    interface IStorage
     {
     }
 }
