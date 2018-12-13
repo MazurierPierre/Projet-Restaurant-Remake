ff447ad15b30ccb281bfabaaff402434042cdf9f
Pierre Mazurier <pierre.mazurier@viacesi.fr>
2018-12-13 16:13:52 +0100

    Initial Commit

---
+++ Unit-Tests/BasicTest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Unit_Tests
+{
+    [TestClass]
+    public class BasicTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.AreEqual(1, 1);
+        }
+    }
+}
