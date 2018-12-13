1379b90f4a45c885411714df28165bb5b116a05a
Pierre Mazurier <pierre.mazurier@viacesi.fr>
2018-12-13 17:34:58 +0100

    Added basic controller classes

---
+++ Controller/Common/Actor.cs
@@ -0,0 +1,34 @@
+﻿using Controller.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Controller.Common
+{
+    class Actor : IActor//, IObserver<T>
+    {
+        public IDisposable cancellation { get; set; }
+        public List<string> itemInfo { get; set; }
+        public Dictionary<string, IAct> actions { get; set; }
+        public IAct iAct { get; set; }
+
+        /*
+        public void OnCompleted()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void OnError(Exception error)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void OnNext(T value)
+        {
+            throw new NotImplementedException();
+        }
+        */
+    }
+}
