31598d3cdde57a6b188fa17fb1d4333c76247057
Pierre Mazurier <pierre.mazurier@viacesi.fr>
2018-12-16 16:18:59 +0100

    Added Comments

--- Controller/Clock/Clock.cs
+++ Controller/Clock/Clock.cs
@@ -10,27 +10,41 @@ namespace Controller.Clock
 {
     class Clock : IClock, IObservable<Clock>
     {
+        // Single instance of clock (Usefull for Singleton pattern)
         private static Clock instance = null;
+
+        // Lock access to instance
         private static readonly object padlock = new object();
 
+        // Is incremented at every clock tick
         private long tickCount;
-        private int timeBetweenTicks;
-        private Timer t = new Timer();
 
+        // Self explanatory ...
+        private int timeBetweenTicks;
+
+        // Timer component
+        private Timer t;
+
+        // Self explanatory ...
         public bool isStarted { get; set; }
         public bool isRestaurantOpen { get; set; }
 
+        // List of objects which observe the clock
         private List<IObserver<Clock>> observers;
 
         private Clock()
         {
+            // Setting default values
             tickCount = 0;
             timeBetweenTicks = 1000;
 
+            // Creating & configuring timer component
+            t = new Timer();
             t.Interval = timeBetweenTicks;
             t.Elapsed += new ElapsedEventHandler(Tick);
         }
 
+        // Singleton pattern : allows only one instance to be created
         public static Clock Instance
         {
             get
@@ -61,37 +75,44 @@ namespace Controller.Clock
 
         public void Tick(object sender, EventArgs e)
         {
+            // Increase tickCount by 1
             tickCount++;
 
+            // Notify all observers
             foreach (var observer in observers)
                 observer.OnNext(this);
         }
 
         public void Warp(int multiplier)
         {
+            // Modify time between ticks
             timeBetweenTicks /= multiplier;
+
+            // Restart clock (!= Reset)
             Stop();
             Start();
         }
 
+        // Reset clock to default values, Does NOT stop the clock.
         public void Reset()
         {
             tickCount = 0;
             timeBetweenTicks = 1000;
-            isStarted = false;
-            isRestaurantOpen = false;
         }
 
+        // Define when the restaurant is closed according to time (in ticks)
         private void openCloseRestaurant()
         {
-
+            // TODO
         }
 
+        // Return the actual clock tickCount
         public long GetTickCount()
         {
             return tickCount;
         }
 
+        // Add observer to the clock
         public IDisposable Subscribe(IObserver<Clock> observer)
         {
             // Check whether observer is already registered. If not, add it
